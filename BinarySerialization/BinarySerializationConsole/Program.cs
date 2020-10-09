dc026b7f31dcf0fcec1f28afaa0669fdc2758196
Anthony Watts <anthonypwatts@gmail.com>
2020-10-09 23:38:51 +0100

    Initial implementation using MessagePack only.

---
+++ BinarySerialization/BinarySerializationConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BinarySerializationConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var objectsToSerialize = new List<SerializationObject>();
+            var deserializedObjects = new List<SerializationObject>();
+            var serializedObjects = new List<byte[]>();
+
+            Console.WriteLine($"{DateTime.Now:mm.ss.fff}: Generating Objects");
+            for (int i = 0; i < 3000000; i++)
+            {
+                objectsToSerialize.Add(new SerializationObject());
+            }
+
+            Console.WriteLine($"{DateTime.Now:mm.ss.fff}: Serializing Objects");
+            foreach (var source in objectsToSerialize)
+            {
+                serializedObjects.Add(MessagePackSerializer.Serialize(source));
+            }
+
+            Console.WriteLine($"{DateTime.Now:mm.ss.fff}: Deserializing Objects");
+            foreach (var serializedObject in serializedObjects)
+            {
+                deserializedObjects.Add(MessagePackSerializer.Deserialize<SerializationObject>(serializedObject));
+
+            }
+
+            Console.WriteLine($"{DateTime.Now:mm.ss.fff}: Finished.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BinarySerialization/BinarySerializationConsole/SerializationObject.cs
@@ -0,0 +1,48 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BinarySerializationConsole
+{
+    [MessagePackObject]
+    public class SerializationObject
+    {
+        public SerializationObject()
+        {
+        }
+
+        public SerializationObject(
+            int integerCount = 10, 
+            int stringCount = 10, 
+            int stringLength = 100, 
+            int dataSizeBytes = megabyte)
+        {
+            for (int i = 0; i < integerCount; i++) this.Integers.Add(_random.Next(1, 100));
+            for (int s = 0; s < stringCount; s++) this.Strings.Add(_generateRandomString(stringLength));
+            this.Data = new byte[dataSizeBytes];
+            _random.NextBytes(this.Data);
+        }
+
+        [Key(0)]
+        public List<int> Integers { get; set; } = new List<int>();
+
+        [Key(1)]
+        public List<string> Strings { get; set; } = new List<string>();
+
+        [Key(2)]
+        public byte[] Data { get; set; }
+
+
+        private const int kilobyte = 1024;
+        private const int megabyte = 1024 * kilobyte;
+
+        private static Random _random = new Random();
+        private static string _generateRandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[_random.Next(s.Length)]).ToArray());
+        }
+    }
+}
